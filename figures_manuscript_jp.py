a2436b0145878131d3d7ac1a00ea7b39ee261c83
Jonathan Platkiewicz <jonathan.platkiewicz@gmail.com>
2017-11-27 01:18:07 -0500

    Add files via upload

--- figures_manuscript_jp.py
+++ figures_manuscript_jp.py
@@ -18,24 +18,24 @@ def plot_figure(figure):
     Input:
         figure: Figure number in the manuscript to plot
     '''
-    if figure == 1:  
-        #-----------------------------------------------------------------------
-        # Figure: Excess synchrony estimate - True synaptic weight relationship
-        #-----------------------------------------------------------------------
-
-        # Generate the spike data
-        p,Tr0,Tt0,Tr,Tt = generate_trains_monosynapse.generate(Ntrial=100,
-                                                               duration=300.)
-        # Analyze the spike data
-        analyze_trains_monosynapse.analyze(p,Tr0,Tt0,Tr,Tt)        
-    elif figure == 2:    
+    if figure == 1:
         #-----------------------------------------------------------------------
         # Figure: Jitter interval length as classification parameter
         #-----------------------------------------------------------------------
 
         # Generate the spike data
-        p,Tr ,Tt0 = generate_timescale_classification.generate(Ntrial=100,
+        p,Tr0,Tt0 = generate_timescale_classification.generate(Ntrial=100,
                                                                duration=300.,
                                                                period=50.)
         # Analyze the spike data
-        analyze_timescale_classification.analyze(p,Tr0,Tt0)
+        analyze_timescale_classification.analyze(p,Tr0,Tt0)   
+    elif figure == 2:    
+        #-----------------------------------------------------------------------
+        # Figure: Excess synchrony estimate - True synaptic weight relationship
+        #-----------------------------------------------------------------------
+
+        # Generate the spike data
+        p,w,Tr,Tt = generate_trains_monosynapse.generate(Ntrial=100,
+                                                         duration=300.)
+        # Analyze the spike data
+        analyze_trains_monosynapse.analyze(p,w,Tr,Tt)    
